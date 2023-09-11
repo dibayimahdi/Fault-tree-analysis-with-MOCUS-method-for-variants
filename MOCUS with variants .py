0f2298481efbed149246168771ada4aa1ce3441d
mahdi dibaei <63470978+dibayimahdi@users.noreply.github.com>
2023-09-11 16:21:26 +0200

    Update MOCUS with variants .py

--- MOCUS with variants .py
+++ MOCUS with variants .py
@@ -11,15 +11,14 @@ supermodel = [("TOP", "Or", ["a", "G1"]),
 
 print("This program computes minimal cut sets for the a variant of a determined supermodel")
 print("If you want to change the supermodel, please changed it inside the code")
-variant = input("Pleas enter the variant variables (a..z), seperated with comma without any space between variables (FOR EXAMPLE: a,b,c)\n")
-parts = variant.split(',')
+variant_variables = input("Pleas enter the variant variables (a..z), seperated with comma without any space between variables (FOR EXAMPLE: a,b,c)\n")
+parts = variant_variables.split(',')
 len_supermodel= len(supermodel)
 lower = list(string.ascii_lowercase)  
 variant1 = deepcopy(supermodel)
+#Assigning zero to those elements of variant that are not in variant 
 for i in range (0, len_supermodel):
    x=supermodel[i]
-   print(x)
-   print(type(x))
    x1=x[2][0]
    x2=x[2][1]
    if ((x1 in lower) and (x1 not in parts)):
@@ -58,18 +57,21 @@ while i< len(variant1):
          else: 
              gate=x1    
          poped= variant1.pop(i)
-         for j in range (i, len(variant1)):
+         for j in range (0, len(variant1)):
             y=variant1[j]
             y1=y[2][0]
             y2=y[2][1]
-            if(y[0]==gate):
-               print(type(variant1[j]))
+            y3=y[2]
+            if(y1==gate1):
                varlist1=list(variant1[j])
-               varlist1[0]=gate1
+               varlist1[2][0]=gate
+               variant1[j]=tuple(varlist1)
+            if(y2==gate1):
+               varlist1=list(variant1[j])
+               varlist1[2][1]=gate
                variant1[j]=tuple(varlist1)
             i=i+1
       i=i+1    
-           
 #Printing Minimal Cut Set for Supermodel and variant 
 print("Minimal Cut Sets for Supermodel is:")
 cs = cutsets.mocus (supermodel)
@@ -77,5 +79,3 @@ print (cs)
 print("Minimal Cut Sets for variant is:")
 cs = cutsets.mocus (variant1)
 print (cs)
-
-
